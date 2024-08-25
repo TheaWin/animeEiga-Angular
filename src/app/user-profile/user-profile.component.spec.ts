f6e7f3028a079b33a064b393567debd425dbd056
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-08-25 10:08:46 +0400

    user-profile unit testing

--- src/app/user-profile/user-profile.component.spec.ts
+++ src/app/user-profile/user-profile.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserProfileComponent } from './user-profile.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { provideHttpClient } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 
 describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
@@ -8,7 +13,13 @@ describe('UserProfileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserProfileComponent]
+      declarations: [UserProfileComponent],
+      imports: [MatSnackBarModule, MatCardModule],
+      providers: [
+        provideRouter([{ path: 'profile', component: UserProfileComponent}]),
+        provideHttpClientTesting(),
+        provideHttpClient()
+      ]
     })
     .compileComponents();
 
