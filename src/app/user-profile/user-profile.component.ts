aec2c56d11c7ab6aa3e7769cb7de0f17a7219839
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-08-25 17:35:40 +0400

    Debug FavMovie in User Profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { GenreDetailComponent } from '../genre-detail/genre-detail.component';
+import { DirectorDetailComponent } from '../director-detail/director-detail.component';
+import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-profile',
@@ -18,6 +22,7 @@ export class UserProfileComponent implements OnInit{
   constructor(
     public fetchApiData: FetchApiDataService,
     public snackBar: MatSnackBar,
+    public dialog: MatDialog,
     private router: Router
   ) { }
 
@@ -81,17 +86,57 @@ export class UserProfileComponent implements OnInit{
     return this.fetchApiData.isFavoriteMovies(movieId);
   }
 
-  addFavoriteMovie(movieId: string): void {
-    this.fetchApiData.addFavoriteMovie(movieId).subscribe(() => {
-      console.log('addFav called');
-      this.snackBar.open('Added to favorites', 'OK', {duration: 2000});
+  deleteFavoriteMovie(movieId: string): void {
+    this.fetchApiData.deleteFavoriteMovie(movieId).subscribe(() => {
+      this.favoriteMovies = this.favoriteMovies.filter((movie: any) => {
+        return movie._id !== movieId;
+      })
+      this.snackBar.open('Removed from favorites', 'OK', {duration: 2000});
+      console.log('del fav called');
+    })
+  }
+
+  openSypnosis(movie: any): void {
+    console.log(movie.Name);
+    this.dialog.open(MovieSynopsisComponent, {
+      data: {
+        name: movie.Name,
+        synopsis: movie.Description,
+        release: movie.releaseYear
+      }
     });
   }
 
-  deleteFavoriteMovie(movieId: string): void {
-    this.fetchApiData.deleteFavoriteMovie(movieId).subscribe(() => {
-      this.snackBar.open('Removed from favorites', 'OK', {duration: 2000});
-      console.log('del fav called');
+  openDirector(movie: any): void {
+    // Function to format date as YYYY-MM-DD
+    const formatDate = (dateString: string | null) => {
+      if (dateString === 'null' || dateString === null) {
+        return 'Null'; // Return 'Unknown' if dateString is 'null' or actual null
+      }
+      if (dateString) {
+        const date = new Date(dateString);
+        return date.toISOString().split('T')[0];
+      }
+      return 'Unknown'; // Return 'Unknown' if dateString is empty
+    };
+  
+    // Open the dialog with the director data
+    this.dialog.open(DirectorDetailComponent, {
+      data: {
+        name: movie.Director.Name,
+        bio: movie.Director.Bio,
+        birth: formatDate(movie.Director.Birth),
+        death: formatDate(movie.Director.Death),
+      }
+    });
+  }
+
+  openGenre(movie: any): void {
+    this.dialog.open(GenreDetailComponent, {
+      data: {
+        name: movie.Genre.Name,
+        description: movie.Genre.Description
+      }
     })
   }
 }
